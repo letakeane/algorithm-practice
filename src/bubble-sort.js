4720c3970e48254b43b033cea9b4cda94017dec3
Leta Keane <leta@hey.com>
2022-05-12 17:11:57 -0600

    Initial commit, first test

---
+++ src/bubble-sort.js
@@ -0,0 +1,5 @@
+const bubbleSort = () => {
+
+}
+
+export default bubbleSort;
---
+++ test/bubble-sort-test.js
@@ -0,0 +1,10 @@
+import bubbleSort from '../src/bubble-sort.js';
+import { assert } from 'chai';
+
+describe('Bubble Sort', () => {
+  it('should be a function', () => {
+    assert.isFunction(bubbleSort);
+  })
+
+  it('should ')
+})
